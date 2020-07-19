945414030725cea16ed5da67ab2e6108ba0e966c
Mukesh Kumar <mkmukeshkumar892@gmail.com>
2020-07-19 12:32:00 +0530

    image processing deployment

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+const jsonServer = require('json-server');
 const app = express();
+const server = jsonServer.create();
+const router = jsonServer.router('api/pics.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/image-processing'));
 
